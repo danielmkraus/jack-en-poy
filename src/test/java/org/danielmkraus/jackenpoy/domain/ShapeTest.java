f7543d91e2728c815cb60deb62c63bcc5f836374
Daniel Machado Kraus <danielmkraus@gmail.com>
2020-10-28 21:19:34 +0100

    creating test to ensure that all cases are covered

--- src/test/java/org/danielmkraus/jackenpoy/domain/ShapeTest.java
+++ src/test/java/org/danielmkraus/jackenpoy/domain/ShapeTest.java
@@ -1,25 +1,25 @@
 package org.danielmkraus.jackenpoy.domain;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatNullPointerException;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.danielmkraus.jackenpoy.domain.MatchResult.*;
+import static org.danielmkraus.jackenpoy.domain.Shape.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class ShapeTest {
 
     @ParameterizedTest(name = "{0} against {1} should {2}")
-    @CsvSource({
-            "ROCK,ROCK,DRAW",
-            "ROCK,SCISSOR,WIN",
-            "ROCK,PAPER,LOOSE",
-            "PAPER,PAPER,DRAW",
-            "PAPER,ROCK,WIN",
-            "PAPER,SCISSOR,LOOSE",
-            "SCISSOR,SCISSOR,DRAW",
-            "SCISSOR,PAPER,WIN",
-            "SCISSOR,ROCK,LOOSE"})
+    @MethodSource("shapesAgainstArguments")
     public void shapes_against(Shape shape, Shape against, MatchResult result) {
         assertThat(shape.against(against)).isEqualTo(result);
     }
@@ -28,5 +28,34 @@ class ShapeTest {
     @ParameterizedTest(name = "{0} against null shape will throw NullPointerException")
     public void against_null_shape(Shape shape) {
         assertThatNullPointerException().isThrownBy(() -> shape.against(null));
+    }
+
+    @Test
+    public void ensure_that_all_possibilities_are_covered(){
+        var allPossibilities = Stream.of(Shape.values()).flatMap(shape -> Stream.of(Shape.values())
+                .map(against -> Map.entry(shape, against)))
+                .collect(Collectors.toSet());
+
+        var mappedPossibilities = shapesAgainstArguments().stream()
+                .map(arguments->
+                        Map.entry((Shape)arguments.get()[0],
+                                (Shape)arguments.get()[1]))
+                .collect(Collectors.toSet());
+
+        assertThat(mappedPossibilities).containsAll(allPossibilities);
+    }
+
+    private static List<Arguments> shapesAgainstArguments(){
+        return List.of(
+                arguments(ROCK,ROCK,DRAW),
+                arguments(ROCK,SCISSOR,WIN),
+                arguments(ROCK,PAPER,LOOSE),
+                arguments(PAPER,PAPER,DRAW),
+                arguments(PAPER,ROCK,WIN),
+                arguments(PAPER,SCISSOR,LOOSE),
+                arguments(SCISSOR,SCISSOR,DRAW),
+                arguments(SCISSOR,PAPER,WIN),
+                arguments(SCISSOR,ROCK,LOOSE)
+        );
     }
 }
